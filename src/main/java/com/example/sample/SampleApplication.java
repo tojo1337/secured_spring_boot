40798f839ddd421cae5d84e3241f0fbf10651326
tojo1337 <tojo.mukherjee007@gmail.com>
2023-06-18 22:36:34 +0530

    5th commit

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -1,13 +1,31 @@
 package com.example.sample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		return args -> {
+			try{
+				Scanner sc = new Scanner(System.in);
+				System.out.print("[*]Enter your name : ");
+				String name = sc.nextLine();
+				System.out.println("[*]Welcome to the application : "+name);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		};
 	}
 
 }
